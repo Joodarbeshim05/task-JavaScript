7fdc0d82c0f920c431f5d2335f62e6ee3a986c37
Joodarbeshim05 <162398776+Joodarbeshim05@users.noreply.github.com>
2024-12-07 23:38:38 +0600

    first JavaScript

---
+++ string.js
@@ -0,0 +1,27 @@
+// string - строка
+// string - "abc" 'abc' `abc`
+
+// string - свойство их 2
+// length
+// index - []
+
+// let str = "welcome";
+// let str = "12345678901234567890";
+// let str = "Bishkek";
+// let s = "city";
+// console.log(str); <|> просто строку покажет
+// console.log(str[3]); <|> покажет 3 индекс
+// console.log(str[str.length - 1]); <|> через эту можно взять последний индекс
+// console.log(str[Math.floor(str.length / 2)]) <|> покажет самый центр;
+// console.log(str.charAt(0)); <|> через номер можно взять строку
+// console.log(str.indexOf("s")); <|> через букву можем узнать в каком индексе стоит
+// console.log(str.lastIndexOf("k"));
+// console.log(str.repeat(500)); <|> повторяет
+// console.log(str.toUpperCase()); <|> сделает всех большим шрифтом
+// console.log(str.toLowerCase()); <|> сделает всех маленьким шрифтом
+// console.log(str.includes("")); <|> boolean
+// console.log(str.replace("h", "d")); <|> меняет букву
+// console.log(str.replaceAll("k", "d"));
+// console.log(str.trim()); <|> удаляет пробелов
+// console.log(str.concat(s));
+// console.log(str.slice(4, 7));
